dfc4c3bda00fbda373c9f1342db40cebde14390a
rahul01r <rrdubeynibm@gmail.com>
2022-11-05 09:20:52 +0530

    [rahul] Add:UC2-DieRoll

--- SnakeAndLadderGame/Game.cs
+++ SnakeAndLadderGame/Game.cs
@@ -8,6 +8,12 @@ namespace SnakeAndLadderGame
 {
     public class Game
     {
-        public int playerposition = 0;
+        public int playerPosition = 0;
+        Random random = new Random();
+        public int DieRoll()
+        {
+            int dieCount = random.Next(1, 7);
+            return dieCount;
+        }
     }
 }
